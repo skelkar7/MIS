ac1ea45c9d05e9bc468ccb5bd9565ac7dbb94cfd
Sojjwal Kelkar <binit7@gmail.com>
2015-04-15 09:23:41 +0200

    corrected some element names

--- app/scripts/controllers/employeedetails.js
+++ app/scripts/controllers/employeedetails.js
@@ -16,17 +16,21 @@ angular.module('myMisAppApp')
 		$scope.leaveFrom = new Date();
 
 		$scope.applyLeaves = function() {
-			$scope.dateRequired = $scope.periodInvalid = false;
+			$scope.dateRequired = $scope.periodInvalid = $scope.leaveApplied = false;
 
 			if($scope.leaveFrom === null || $scope.leaveTo === null)
 			{
 				$scope.dateRequired = true;
 				return;
 			}
-			if($scope.leaveFrom > $scope.leaveTo)
+			else if($scope.leaveFrom > $scope.leaveTo)
 			{
 				$scope.periodInvalid = true;
 				return;
+			}
+			else
+			{
+				$scope.leaveApplied = true;
 			}
 
 			$scope.leaveDays = Math.ceil(($scope.leaveTo - $scope.leaveFrom)/(1000 * 60 * 60 * 24)) + 1;
