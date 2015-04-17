af14002ee733ab581253df6b4e299ea9b1d9c652
Sojjwal Kelkar <s.kelkar@maxxton.com>
2015-04-17 14:09:36 +0530

    test for LoginCtrl - UNFINISHED - testing $http promise not working properly yet

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,33 +10,39 @@
 angular.module('myMisAppApp')
   .controller('LoginCtrl', function ($scope, EmployeeDumpService, LoginService) {
 
-		EmployeeDumpService.getEmployeeData().success(function(data) {
-			$scope.employees = data;
-		});
+    $scope.employees = null;
+    
+    $scope.loadEmployees = function() {
+      EmployeeDumpService.getEmployeeData().success(function(data) {
+        $scope.employees = data;
+      });
+    };
+    
+    $scope.loadEmployees();
+    
+    $scope.showLoginWindow = true;
+    $scope.invalidLogin = false;
 
-		$scope.showLoginWindow = true;
-		$scope.invalidLogin = false;
+    $scope.login = function(username, password) {
+      LoginService.setCurrEmployee(null);
+      angular.forEach($scope.employees, function(employee) {
+        if(employee.username === username && employee.password === password)
+        {
+          LoginService.broadcast(employee);
+          $scope.showLoginWindow = false;
+          $scope.invalidLogin = false;
+        }
+      });
 
-		$scope.login = function() {
-			LoginService.setCurrEmployee(null);
-			angular.forEach($scope.employees, function(employee) {
-				if(employee.username === $scope.username && employee.password === $scope.password)
-				{
-					LoginService.broadcast(employee);
-					$scope.showLoginWindow = false;
-					$scope.invalidLogin = false;
-				}
-			});
+      if(LoginService.getCurrEmployee() === null)
+      {
+        $scope.invalidLogin = true;
+      }
+      $scope.username = $scope.password = null;
+    };
 
-			if(LoginService.getCurrEmployee() === null)
-			{
-				$scope.invalidLogin = true;
-			}
-			$scope.username = $scope.password = null;
-		};
-
-		$scope.logout = function() {
-			LoginService.broadcast(null);
-			$scope.showLoginWindow = true;
-		};
+    $scope.logout = function() {
+      LoginService.broadcast(null);
+      $scope.showLoginWindow = true;
+    };
   });
--- test/spec/controllers/login.js
+++ test/spec/controllers/login.js
@@ -1,22 +1,44 @@
 'use strict';
 
+
 describe('Controller: LoginCtrl', function () {
+  var fakeEmp, loginCtrl, scope, rootScope, empDumpService;
 
-  // load the controller's module
+  fakeEmp = [{
+    "username": "uu",
+    "password": "pp"
+  }];
+    
   beforeEach(module('myMisAppApp'));
-
-  var LoginCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    LoginCtrl = $controller('LoginCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  
+  beforeEach(function() {
+    inject(function($controller, $rootScope, $q, EmployeeDumpService) {
+      rootScope = $rootScope;
+      empDumpService = EmployeeDumpService;
+      
+      //$http's get method returns a promise. $q is some library to deal with promises. 
+      var deferred = $q.defer();
+            
+      deferred.resolve(fakeEmp);
+      
+      spyOn(EmployeeDumpService, 'getEmployeeData').and.returnValue(deferred.promise);
+      scope = $rootScope.$new();
+      
+      loginCtrl = $controller('LoginCtrl', {
+        $scope: scope
+      });        
+    });  
   });
+  
+  it('getEmployeeData function of service is called', function() {
+    scope.loadEmployees();
+    rootScope.$apply();
+    expect(empDumpService.getEmployeeData).toHaveBeenCalled();
+  });
+  
+  it('should load the fake data', function() {
+    scope.loadEmployees();
+    rootScope.$apply();
+    expect(scope.employees).toBe(fakeEmp);
+  });  
 });
